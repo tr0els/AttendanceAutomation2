e59b2940cd65aa78ced41c758cdab7b950df2aef
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2020-04-15 16:32:36 +0200

    Update StudViewController.java

--- src/attendanceautomation/GUI/Controller/StudViewController.java
+++ src/attendanceautomation/GUI/Controller/StudViewController.java
@@ -124,7 +124,7 @@ public class StudViewController implements Initializable {
         handlePieChart();
         handleBarChart();
         handleMissedDays();
-    }
+    }}
 
     public void handlePieChart() {
 
