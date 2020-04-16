1b56a66b80e2094f537c7903a7cdfc307ddf67fb
Guruen <56912822+Guruen@users.noreply.github.com>
2020-04-16 12:04:04 +0200

    Update StudViewController.java

--- src/attendanceautomation/GUI/Controller/StudViewController.java
+++ src/attendanceautomation/GUI/Controller/StudViewController.java
@@ -184,21 +184,7 @@ public class StudViewController implements Initializable {
 
         lblStudentFullname.setText(CurrentStudent.getName());
         
-        
-        daysPresent = 15; //hvor langt tilbage listen over missed days viser
         lblMissedDays.setText("Missed Days (Last " + daysPresent + " days)");
-
-        absence = model.studentAbsence(personID);
-
-        currentDate = model.getCurrentDate();
-        String strDate = currentDate.format(DateTimeFormatter.ofPattern("dd. MMMM yyyy"));
-        showDate.setText(strDate);
-
-        registeredToday = model.studentAlreadyRegistered(personID);
-        if (registeredToday != null) {
-            btnAttendCurrentClass.setDisable(true);
-            btnAttendCurrentClass.setText(registeredToday);
-        }
 
         handlePieChart();
         handleBarChart(CurrentStudent.getPersonID());
